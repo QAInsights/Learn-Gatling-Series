8c2826fcc1e8c083de5a4376c8681cb0ace35214
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2022-04-23 18:32:14 -0400

    Gatling Conf

---
+++ GradleGatlingDemo/src/gatling/java/config/ConfigDemo.java
@@ -0,0 +1,39 @@
+package config;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class ConfigDemo extends Simulation {
+
+    HttpProtocolBuilder req = http
+            .baseUrl("https://onlineboutique.dev");
+
+    ScenarioBuilder myscenario = scenario("OnlineBoutique Home Page")        // Scenario Creation
+            .exec(http("T00_Home").get("/"));                                // Attaching methods
+
+    ScenarioBuilder multipleRequests = scenario("MultipleRequests")
+            .exec(http("T10_Product").get("/product/OLJCESPC7Z"))            // Multiple methods
+            .exec(http("T20_Checkout").get("/checkout"))
+            .pause(3);
+
+    ChainBuilder contact = exec(http("T30_Contact").get("/contact"))            // Composable requests
+            .pause(3);
+
+    ChainBuilder continueShopping = exec(http("T40_ContinueShopping").get("/"))    // Composable requests
+            .pause(3);
+
+    ScenarioBuilder contactAndContinueScenario = scenario("Contact and Continue Shopping") // Scenario  for composable reqs
+            .exec(contact, continueShopping);
+
+    {
+        setUp(
+                myscenario.injectOpen(atOnceUsers(1)),
+                multipleRequests.injectOpen(atOnceUsers(1)),
+                contactAndContinueScenario.injectOpen(atOnceUsers(1))
+        ).protocols(req);
+    }
+
+}
