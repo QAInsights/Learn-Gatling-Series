da97ea9fcff0ba41f83e4ae7f87c272c5ac706fc
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2022-04-29 22:54:11 -0400

    Add: Correlation

--- GradleGatlingDemo/src/gatling/java/correlation/CorrelationDemo.java
+++ GradleGatlingDemo/src/gatling/java/correlation/CorrelationDemo.java
@@ -5,9 +5,6 @@ import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
 import java.math.BigDecimal;
-import java.text.NumberFormat;
-import java.util.Currency;
-import java.util.Locale;
 import java.util.Random;
 import java.util.UUID;
 
@@ -24,48 +21,82 @@ public class CorrelationDemo extends Simulation {
             .exec(http("T00_Home").get("/"));
 
     ScenarioBuilder loginPage = scenario("Login")
-            .exec(http("T10_Product").post("/login")
+            .exec(http("T10_LoggedIn").post("/login")
                     .formParam("username","testuser")
                     .formParam("password","password")
+                    .check(
+                            regex("<span class=\\\"(.*)\\\" id=\\\"current-balance\\\">((.|\\n)+?)<\\/span>").captureGroups(2).saveAs("beforeTransferBalance")
+                    )
             )
             .pause(1);
-
     Random rdm = new Random();
     Integer amountToTransfer = rdm.nextInt(1000) + 1;
 
-    ScenarioBuilder transferFunds = scenario("Add To Cart")
+    ScenarioBuilder transferFunds = scenario("Transfer Funds")
+            .exec(
+                    session -> {
+                        // Check the balance
+                        String balanceCheck = session.getList("beforeTransferBalance").get(1).toString().trim();
+                        balanceCheck = balanceCheck.substring(1);
+
+                        BigDecimal balanceCheckFormat = new BigDecimal(balanceCheck.replaceAll(",", ""));
+                        Integer res = balanceCheckFormat.compareTo(BigDecimal.valueOf(1000));
+
+                        if (res <= 0) {
+                            System.out.println("Not enough balance.");
+                        }
+                        else {
+                            System.out.println("Proceeding to transfer.");
+                        }
+                        return session;
+                    }
+            )
+
             .exec(http("T20_TransferFunds").post("/payment")
                     .formParam("account_num","9879879870")
                     .formParam("contact_account_num","")
                     .formParam("contact_label","")
-                    .formParam("amount",amountToTransfer)
+                    .formParam("amount", amountToTransfer)
                     .formParam("uuid",UUID.randomUUID())
                     .check(
                             status().is(200),
                             substring("Payment successful"),
-                            regex("<span class=\\\"(.*)\\\" id=\\\"current-balance\\\">((.|\\n)+?)<\\/span>").captureGroups(2).saveAs("currentBalance")
-                    )
-            )
+                            regex("<span class=\\\"(.*)\\\" id=\\\"current-balance\\\">((.|\\n)+?)<\\/span>").captureGroups(2).saveAs("afterBalance")
+                    ))
             .exec(session -> {
-                String getBalance = session.getList("currentBalance").get(1).toString().trim();
-                getBalance = getBalance.substring(1);
+                String afterBalance = session.getList("afterBalance").get(1).toString().trim();
+                afterBalance = afterBalance.substring(1);
 
-                BigDecimal money = new BigDecimal(getBalance.replaceAll(",", ""));
-                System.out.println("Money" + money);
+                BigDecimal afterBalanceFormat = new BigDecimal(afterBalance.replaceAll(",", ""));
 
+                String beforeTransfer = session.getList("beforeTransferBalance").get(1).toString().trim();
+                beforeTransfer = beforeTransfer.substring(1);
+
+                BigDecimal beforeTransferFormat = new BigDecimal(beforeTransfer.replaceAll(",", ""));
+
+                // Finding whether the balance is matching or not, after transferring
+                if (beforeTransferFormat.subtract(BigDecimal.valueOf(amountToTransfer)).equals(afterBalanceFormat)) {
+                    System.out.println("Balance is matching.");
+                }
+                else {
+                    System.out.println("Balance is not matching");
+                }
                 return session;
 
             })
+
             .pause(1);
 
+    ScenarioBuilder logout = scenario("logout")
+            .exec(http("T30_LogOut").post("/logout")
+            )
+            .pause(1);
 
-
-    ScenarioBuilder allTransaction = scenario("Online")
-            .exec(homePage, loginPage, transferFunds);
-
+    ScenarioBuilder allTransactions = scenario("BankOfAnthos")
+            .exec(homePage, loginPage, transferFunds,logout);
     {
         setUp(
-            allTransaction.injectOpen(atOnceUsers(1))
+            allTransactions.injectOpen(atOnceUsers(1))
         ).protocols(req);
     }
 
